9ad61b1d017148d9ef20684326e8170e05a728bf
Derrick Nyasio <dnyasio2014@gmail.com>
2022-10-07 17:06:32 +0300

    Routing Navigation functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 function Home() {
-  return <h1>Welcomde to Funland Toy Store!</h1>;
+  return <>
+  <h1>Welcomde to Funland Toy Store!</h1>;
+  
+  </>
 }
 
 export default Home;

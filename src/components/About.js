0b438284254350485eeebd453cbce9cbc926f350
Derrick Nyasio <dnyasio2014@gmail.com>
2022-10-07 17:04:10 +0300

    Updated About Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function About() {
-  return <h1>This is my about component!</h1>;
+  return <>
+  <h1>This is my about component!</h1>
+  <p>
+    We are a fun toy shop equipped with all your favorite toys. Hurry up and visit our toys page to see available toys!
+  </p>
+  </>;
 }
 
 export default About;
